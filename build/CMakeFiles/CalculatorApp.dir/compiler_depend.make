71212081bedcee553f4c679c3f8253204e3518ac
Sachin Kolachana-kolacs1 <sachin.kolachana@cat.com>
2023-12-08 00:32:56 -0500

    removed build dir

--- build/CMakeFiles/CalculatorApp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for CalculatorApp.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/u_kolacs1/host/Downloads/calculator_task
-# Build directory: /home/u_kolacs1/host/Downloads/calculator_task/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

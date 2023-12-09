946413d66acbef1447fdc41d5a2eaae4a445aac2
Sachin Kolachana-kolacs1 <sachin.kolachana@cat.com>
2023-12-09 01:37:25 -0500

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/u_kolacs1/host/Downloads/calculator_task
-# Build directory: /home/u_kolacs1/host/Downloads/calculator_task/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/u_kolacs1/host/Downloads/calculator_task/build/test_functions[1]_include.cmake")
